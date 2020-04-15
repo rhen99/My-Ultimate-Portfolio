b1435cac508176ec93b8ed5cce6dfb8cd4af5c31
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-04-15 15:17:10 +0800

    added scroll animations on portfolio page

--- dist/js/dom manipulation.js
+++ dist/js/dom manipulation.js
@@ -1,6 +1,7 @@
 //Toggle Menu
 const menu = document.querySelector(".secondary-navigation");
 const optBtn = document.querySelectorAll(".opt-btn");
+const header = document.querySelector(".header");
 
 optBtn[0].addEventListener("click", () => {
   menu.classList.add("show");
@@ -10,3 +11,15 @@ optBtn[1].addEventListener("click", () => {
   menu.classList.remove("show");
   optBtn[0].classList.remove("opened");
 });
+const scroll = new SmoothScroll('.side-navigation a[href*="#"]', {
+  speed: 500,
+});
+window.addEventListener("scroll", navbarSnap);
+
+function navbarSnap() {
+  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
