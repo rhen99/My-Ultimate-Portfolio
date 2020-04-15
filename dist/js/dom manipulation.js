107727028239c607a3360bb3d262d49efe6cf206
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-04-15 16:58:13 +0800

    portfolio page done! 2 more pages to go

--- dist/js/dom manipulation.js
+++ dist/js/dom manipulation.js
@@ -2,6 +2,7 @@
 const menu = document.querySelector(".secondary-navigation");
 const optBtn = document.querySelectorAll(".opt-btn");
 const header = document.querySelector(".header");
+const sectionLinks = document.querySelectorAll(".side-navigation a");
 
 optBtn[0].addEventListener("click", () => {
   menu.classList.add("show");
@@ -15,6 +16,7 @@ const scroll = new SmoothScroll('.side-navigation a[href*="#"]', {
   speed: 500,
 });
 window.addEventListener("scroll", navbarSnap);
+window.addEventListener("scroll", scrollActive);
 
 function navbarSnap() {
   if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
@@ -22,4 +24,19 @@ function navbarSnap() {
   } else {
     header.classList.remove("sticky");
   }
+}
+function scrollActive() {
+  const fromTop = window.scrollY;
+  sectionLinks.forEach((link) => {
+    const section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
 }
