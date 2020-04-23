4b376adec86a6f3f66d4827e6969560d076e74b6
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-04-23 18:18:03 +0800

    cleaned css, smaller devices added contact copy to clipboard functionality added

--- dist/js/dom manipulation.js
+++ dist/js/dom manipulation.js
@@ -3,7 +3,14 @@ const menu = document.querySelector(".secondary-navigation");
 const optBtn = document.querySelectorAll(".opt-btn");
 const header = document.querySelector(".header");
 const sectionLinks = document.querySelectorAll(".side-navigation a");
+//Contact Infos
+const copyNum = document.querySelector(".copy-number");
+const copySkype = document.querySelector(".copy-skype");
+const phoneNumber = document.querySelector("#number");
+const skypeHandle = document.querySelector("#skype");
+const copyText = document.querySelectorAll(".copied-text");
 
+//Secondary Menu Toggle EventListeners / Functions
 optBtn[0].addEventListener("click", () => {
   menu.classList.add("show");
   optBtn[0].classList.add("opened");
@@ -12,12 +19,15 @@ optBtn[1].addEventListener("click", () => {
   menu.classList.remove("show");
   optBtn[0].classList.remove("opened");
 });
+
+//Use SmoothScroll for portfolio page.
 const scroll = new SmoothScroll('.side-navigation a[href*="#"]', {
   speed: 500,
 });
+// Scrolling EventListeners
 window.addEventListener("scroll", navbarSnap);
 window.addEventListener("scroll", scrollActive);
-
+//Sticky Navbar Function
 function navbarSnap() {
   if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
     header.classList.add("sticky");
@@ -25,6 +35,7 @@ function navbarSnap() {
     header.classList.remove("sticky");
   }
 }
+//Jump Scroll Function
 function scrollActive() {
   const fromTop = window.scrollY;
   sectionLinks.forEach((link) => {
@@ -39,4 +50,21 @@ function scrollActive() {
       link.classList.remove("current");
     }
   });
+}
+copyNum.addEventListener("click", copyPhone);
+copySkype.addEventListener("click", copySkypeHandle);
+//Copy Phone Number
+function copyPhone() {
+  copyText[1].classList.add("copied");
+  setTimeout(() => copyText[1].classList.remove("copied"), 2000);
+  phoneNumber.select();
+  phoneNumber.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+}
+function copySkypeHandle() {
+  copyText[0].classList.add("copied");
+  setTimeout(() => copyText[0].classList.remove("copied"), 2000);
+  skypeHandle.select();
+  skypeHandle.setSelectionRange(0, 99999);
+  document.execCommand("copy");
 }
